96d01111c81c6bbaa035e34d3fe0dcafa4b1ac19
James Knight <tech@jamesknightcoaching.com>
2021-02-13 00:27:19 +0000

    Trying again, with a different theme.

--- .eleventy.js
+++ .eleventy.js
@@ -1,52 +1,63 @@
-const { DateTime } = require("luxon");
-const fs = require("fs");
-const pluginRss = require("@11ty/eleventy-plugin-rss");
-const pluginSyntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
-const pluginNavigation = require("@11ty/eleventy-navigation");
-const markdownIt = require("markdown-it");
-const markdownItAnchor = require("markdown-it-anchor");
-const cacheBuster = require("@mightyplow/eleventy-plugin-cache-buster");
-const htmlmin = require("html-minifier");
-const env = require("./src/_data/env");
+const { DateTime } = require('luxon');
+const readingTime = require('eleventy-plugin-reading-time');
+const pluginRss = require('@11ty/eleventy-plugin-rss');
+const syntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight');
+const fs = require('fs');
+const path = require('path');
 
-module.exports = function(eleventyConfig) {
+const isDev = process.env.APP_ENV === 'development';
+
+const manifestPath = path.resolve(
+  __dirname,
+  'public',
+  'assets',
+  'manifest.json'
+);
+
+const manifest = isDev
+  ? {
+      'main.js': '/assets/main.js',
+      'main.css': '/assets/main.css',
+    }
+  : JSON.parse(fs.readFileSync(manifestPath, { encoding: 'utf8' }));
+
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPlugin(readingTime);
   eleventyConfig.addPlugin(pluginRss);
-  eleventyConfig.addPlugin(pluginSyntaxHighlight);
-  eleventyConfig.addPlugin(pluginNavigation);
-
-  if (env.production) {
-    eleventyConfig.addPlugin(cacheBuster({}));
-
-    eleventyConfig.addTransform("htmlmin", function(content, outputPath) {
-      if (outputPath.endsWith(".html")) {
-        return htmlmin.minify(content, {
-          useShortDoctype: true,
-          removeComments: true,
-          collapseWhitespace: true
-        });
-      } else {
-        return content;
-      }
-    });
-  }
+  eleventyConfig.addPlugin(syntaxHighlight);
 
   eleventyConfig.setDataDeepMerge(true);
+  eleventyConfig.addPassthroughCopy({ 'src/images': 'images' });
+  eleventyConfig.setBrowserSyncConfig({ files: [manifestPath] });
 
-  eleventyConfig.addLayoutAlias("post", "layouts/post.njk");
+  eleventyConfig.addShortcode('bundledcss', function () {
+    return manifest['main.css']
+      ? `<link href="${manifest['main.css']}" rel="stylesheet" />`
+      : '';
+  });
 
-  eleventyConfig.addFilter("readableDate", dateObj => {
-    return DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat(
-      "dd LLL yyyy"
+  eleventyConfig.addShortcode('bundledjs', function () {
+    return manifest['main.js']
+      ? `<script src="${manifest['main.js']}"></script>`
+      : '';
+  });
+
+  eleventyConfig.addFilter('excerpt', (post) => {
+    const content = post.replace(/(<([^>]+)>)/gi, '');
+    return content.substr(0, content.lastIndexOf(' ', 200)) + '...';
+  });
+
+  eleventyConfig.addFilter('readableDate', (dateObj) => {
+    return DateTime.fromJSDate(dateObj, { zone: 'utc' }).toFormat(
+      'dd LLL yyyy'
     );
   });
 
-  // https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
-  eleventyConfig.addFilter("htmlDateString", dateObj => {
-    return DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat("yyyy-LL-dd");
+  eleventyConfig.addFilter('htmlDateString', (dateObj) => {
+    return DateTime.fromJSDate(dateObj, { zone: 'utc' }).toFormat('yyyy-LL-dd');
   });
 
-  // Get the first `n` elements of a collection.
-  eleventyConfig.addFilter("head", (array, n) => {
+  eleventyConfig.addFilter('head', (array, n) => {
     if (n < 0) {
       return array.slice(n);
     }
@@ -54,23 +65,18 @@ module.exports = function(eleventyConfig) {
     return array.slice(0, n);
   });
 
-  eleventyConfig.addFilter("min", (...numbers) => {
-    return Math.min.apply(null, numbers);
-  });
-
-  eleventyConfig.addCollection("tagList", function(collection) {
+  eleventyConfig.addCollection('tagList', function (collection) {
     let tagSet = new Set();
-    collection.getAll().forEach(function(item) {
-      if ("tags" in item.data) {
+    collection.getAll().forEach(function (item) {
+      if ('tags' in item.data) {
         let tags = item.data.tags;
 
-        tags = tags.filter(function(item) {
+        tags = tags.filter(function (item) {
           switch (item) {
-            // this list should match the `filter` list in tags.njk
-            case "all":
-            case "nav":
-            case "post":
-            case "posts":
+            case 'all':
+            case 'nav':
+            case 'post':
+            case 'posts':
               return false;
           }
 
@@ -83,65 +89,31 @@ module.exports = function(eleventyConfig) {
       }
     });
 
-    // returning an array in addCollection works in Eleventy 0.5.3
     return [...tagSet];
   });
 
-  eleventyConfig.addPassthroughCopy("src/img");
-  eleventyConfig.addPassthroughCopy("src/css");
+  eleventyConfig.addFilter('pageTags', (tags) => {
+    const generalTags = ['all', 'nav', 'post', 'posts'];
 
-  /* Markdown Overrides */
-  let markdownLibrary = markdownIt({
-    html: true,
-    breaks: true,
-    linkify: true
-  }).use(markdownItAnchor, {
-    permalink: true,
-    permalinkClass: "direct-link",
-    permalinkSymbol: "#"
-  });
-  eleventyConfig.setLibrary("md", markdownLibrary);
-
-  // Browsersync Overrides
-  eleventyConfig.setBrowserSyncConfig({
-    callbacks: {
-      ready: function(err, browserSync) {
-        const content_404 = fs.readFileSync("_site/404.html");
-
-        browserSync.addMiddleware("*", (req, res) => {
-          // Provides the 404 content without redirect.
-          res.write(content_404);
-          res.end();
-        });
-      }
-    },
-    ui: false,
-    ghostMode: false
+    return tags
+      .toString()
+      .split(',')
+      .filter((tag) => {
+        return !generalTags.includes(tag);
+      });
   });
 
   return {
-    templateFormats: ["md", "njk", "html", "liquid"],
-
-    // If your site lives in a different subdirectory, change this.
-    // Leading or trailing slashes are all normalized away, so don’t worry about those.
-
-    // If you don’t have a subdirectory, use "" or "/" (they do the same thing)
-    // This is only used for link URLs (it does not affect your file structure)
-    // Best paired with the `url` filter: https://www.11ty.dev/docs/filters/url/
-
-    // You can also pass this in on the command line using `--pathprefix`
-    // pathPrefix: "/",
-
-    markdownTemplateEngine: "liquid",
-    htmlTemplateEngine: "njk",
-    dataTemplateEngine: "njk",
-
-    // These are all optional, defaults are shown:
     dir: {
-      input: "./src",
-      includes: "_includes",
-      data: "_data",
-      output: "_site"
-    }
+      input: 'src',
+      output: 'public',
+      includes: 'includes',
+      data: 'data',
+      layouts: 'layouts',
+      passthroughFileCopy: true,
+      templateFormats: ['html', 'njk', 'md'],
+      htmlTemplateEngine: 'njk',
+      markdownTemplateEngine: 'njk',
+    },
   };
 };
--- src/_data/env.js
+++
@@ -1,7 +0,0 @@
-require("dotenv").config();
-
-module.exports = {
-  production: process.env.ELEVENTY_ENV === "production",
-  development: process.env.ELEVENTY_ENV === "development",
-  env: process.env.ELEVENTY_ENV || "development"
-};
