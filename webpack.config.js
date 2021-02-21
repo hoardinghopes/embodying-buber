f1f7fab4f1f132bfc579db4d4835cd019ce6b125
James Knight <tech@jamesknightcoaching.com>
2021-02-21 13:59:26 +0000

    updating path to javascript and css entrypoints for webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,19 @@
-const path = require('path');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const { WebpackManifestPlugin } = require('webpack-manifest-plugin');
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { WebpackManifestPlugin } = require("webpack-manifest-plugin");
 
-const isDev = process.env.APP_ENV === 'development';
+const isDev = process.env.APP_ENV === "development";
 
-const baseFilename = isDev ? 'main' : 'main.[contenthash]';
+const baseFilename = isDev ? "main" : "main.[contenthash]";
 
 module.exports = {
   entry: [
-    path.resolve(__dirname, 'src', 'js', 'main.js'),
-    path.resolve(__dirname, 'src', 'css', 'main.css'),
+    path.resolve(__dirname, "src", "assets", "js", "main.js"),
+    path.resolve(__dirname, "src", "assets", "css", "main.css")
   ],
   output: {
-    path: path.resolve(__dirname, 'public', 'assets'),
-    filename: `${baseFilename}.js`,
+    path: path.resolve(__dirname, "public", "assets"),
+    filename: `${baseFilename}.js`
   },
 
   module: {
@@ -23,31 +23,31 @@ module.exports = {
         exclude: /node_modules/,
         use: [
           {
-            loader: 'babel-loader',
+            loader: "babel-loader",
             options: {
-              presets: ['@babel/preset-env'],
-            },
-          },
-        ],
+              presets: ["@babel/preset-env"]
+            }
+          }
+        ]
       },
       {
         test: /\.css$/,
         use: [
           MiniCssExtractPlugin.loader,
           {
-            loader: 'css-loader',
+            loader: "css-loader",
             options: {
-              importLoaders: 1,
-            },
+              importLoaders: 1
+            }
           },
-          'postcss-loader',
-        ],
-      },
-    ],
+          "postcss-loader"
+        ]
+      }
+    ]
   },
 
   plugins: [
-    new WebpackManifestPlugin({ publicPath: '/assets/' }),
-    new MiniCssExtractPlugin({ filename: `${baseFilename}.css` }),
-  ],
+    new WebpackManifestPlugin({ publicPath: "/assets/" }),
+    new MiniCssExtractPlugin({ filename: `${baseFilename}.css` })
+  ]
 };
