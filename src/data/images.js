6bc5bf336f27fdfc6a62254c5620bdd124d4ac28
James Knight <tech@jamesknightcoaching.com>
2021-02-23 06:57:04 +0000

    Day 9: Centering. Ready.

--- src/data/images.js
+++ src/data/images.js
@@ -2,70 +2,71 @@ module.exports = {
   test: {
     src: "/images/backgrounds/v2osk-Chi53DEUJ84-unsplash.jpg",
     attribute:
-      '<span>Background photo by <a href="https://unsplash.com/@v2osk?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">v2osk</a> on <a href="https://unsplash.com/s/visual/0c296cda-2158-4cc9-843d-7b1792091157?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Unsplash</a></span>'
+      '<span>Background photo by <a href="https://unsplash.com/@v2osk?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">v2osk</a> on <a href="https://unsplash.com/s/visual/0c296cda-2158-4cc9-843d-7b1792091157?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Unsplash</a></span>',
   },
   about: {
     src: "/images/backgrounds/v2osk-Chi53DEUJ84-unsplash.jpg",
     attribute:
-      '<span>Background photo by <a href="https://unsplash.com/@v2osk?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">v2osk</a> on <a href="https://unsplash.com/s/visual/0c296cda-2158-4cc9-843d-7b1792091157?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Unsplash</a></span>'
+      '<span>Background photo by <a href="https://unsplash.com/@v2osk?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">v2osk</a> on <a href="https://unsplash.com/s/visual/0c296cda-2158-4cc9-843d-7b1792091157?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Unsplash</a></span>',
   },
   contact: {
     src: "/images/backgrounds/camille-orgel-SKcTKYNRvHY-unsplash.jpg",
     attribute:
-      '<span>Background photo by <a href="https://unsplash.com/@cam_bam?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Camille Orgel</a> on <a href="https://unsplash.com/s/photos/writing-letter?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Unsplash</a></span>'
+      '<span>Background photo by <a href="https://unsplash.com/@cam_bam?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Camille Orgel</a> on <a href="https://unsplash.com/s/photos/writing-letter?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Unsplash</a></span>',
   },
   thanks: {
     src: "/images/backgrounds/sandra-grunewald-pL6Z17faozk-unsplash.jpg",
     attribute:
-      '<span>Background photo by <a href="https://unsplash.com/@elmuff?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Sandra Grünewald</a> on <a href="https://unsplash.com/s/photos/hearts?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Unsplash</a></span>'
+      '<span>Background photo by <a href="https://unsplash.com/@elmuff?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Sandra Grünewald</a> on <a href="https://unsplash.com/s/photos/hearts?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Unsplash</a></span>',
   },
   "post-1": {
     src: "/images/backgrounds/mel-R4bsmYIc6FI-unsplash.jpg",
     attribute:
-      '<span>Background photo by <a href="https://unsplash.com/@melindapack?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Mel</a> on <a href="https://unsplash.com/s/visual/131065db-23df-45b0-a58f-0024280938f8?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Unsplash</a></span>'
+      '<span>Background photo by <a href="https://unsplash.com/@melindapack?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Mel</a> on <a href="https://unsplash.com/s/visual/131065db-23df-45b0-a58f-0024280938f8?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Unsplash</a></span>',
   },
   "post-2": {
     src: "/images/backgrounds/sharon-mccutcheon-om4O_x_qWD8-unsplash.jpg",
     attribute:
-      '<span>Background photo by <a href="https://unsplash.com/@sharonmccutcheon?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Sharon McCutcheon</a> on <a href="https://unsplash.com/s/visual/a90c41e3-9993-47c7-8e84-34e01750628b?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Unsplash</a></span>'
+      '<span>Background photo by <a href="https://unsplash.com/@sharonmccutcheon?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Sharon McCutcheon</a> on <a href="https://unsplash.com/s/visual/a90c41e3-9993-47c7-8e84-34e01750628b?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Unsplash</a></span>',
   },
 
   "post-3": {
     src:
       "/images/backgrounds/city-church-christchurch-0sfPFlBcciA-unsplash.jpg",
     attribute:
-      '<span>Background photo by <a href="https://unsplash.com/@citychurchchristchurch?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">City Church Christchurch</a> on <a href="https://unsplash.com/s/photos/swarm?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Unsplash</a></span>'
+      '<span>Background photo by <a href="https://unsplash.com/@citychurchchristchurch?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">City Church Christchurch</a> on <a href="https://unsplash.com/s/photos/swarm?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Unsplash</a></span>',
   },
   "post-4": {
     src: "/images/backgrounds/ankush-minda-TLBplYQvqn0-unsplash.jpg",
     attribute:
-      '<span>Background photo by <a href="https://unsplash.com/@an_ku_sh?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Ankush Minda</a> on <a href="https://unsplash.com/s/photos/release?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Unsplash</a></span>'
+      '<span>Background photo by <a href="https://unsplash.com/@an_ku_sh?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Ankush Minda</a> on <a href="https://unsplash.com/s/photos/release?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Unsplash</a></span>',
   },
   "post-5": {
     src: "/images/backgrounds/milada-vigerova-0ArA_q8Gjt8-unsplash.jpg",
     attribute:
-      '<span>Background photo by <a href="https://unsplash.com/@milada_vigerova?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Milada Vigerova</a> on <a href="https://unsplash.com/s/photos/heavy-ropes?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Unsplash</a></span>'
+      '<span>Background photo by <a href="https://unsplash.com/@milada_vigerova?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Milada Vigerova</a> on <a href="https://unsplash.com/s/photos/heavy-ropes?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Unsplash</a></span>',
   },
   "post-6": {
     src: "/images/backgrounds/david-clode-1UBIvjKygYc-unsplash.jpg",
     attribute:
-      '<span>Photo by <a href="https://unsplash.com/@davidclode?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">David Clode</a> on <a href="https://unsplash.com/?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Unsplash</a></span>'
+      '<span>Photo by <a href="https://unsplash.com/@davidclode?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">David Clode</a> on <a href="https://unsplash.com/?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Unsplash</a></span>',
   },
   "post-7": {
     src: "/images/backgrounds/susan-yin-Ctaj_HCqW84-unsplash.jpg",
     attribute:
-      '<span>Background photo by <a href="https://unsplash.com/@syinq?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Susan Yin</a> on <a href="https://unsplash.com/s/photos/maze?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Unsplash</a></span>'
+      '<span>Background photo by <a href="https://unsplash.com/@syinq?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Susan Yin</a> on <a href="https://unsplash.com/s/photos/maze?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Unsplash</a></span>',
   },
   "post-8": {
     src: "/images/backgrounds/sistine-chapel-hand-of-god.jpg",
-    attribute: ""
+    attribute: "",
   },
   "post-9": {
-    src: "",
-    attribute: ""
+    src: "/images/backgrounds/marek-piwnicki-S07d0_p_xag-unsplash.jpg",
+    attribute:
+      '<span>Background photo by <a href="https://unsplash.com/@marekpiwnicki?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Marek Piwnicki</a> on <a href="https://unsplash.com/s/photos/spinning-top?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Unsplash</a></span>',
   },
   "post-10": {
     src: "/images/backgrounds/blue-yoga-block.jpg",
-    attribute: ""
-  }
+    attribute: "",
+  },
 };
---
+++ src/posts/post-10.md
@@ -0,0 +1,50 @@
+---
+date: 2021-02-24
+title: Of wanting and not-wanting
+description: "As Treebeard said: come back to me! Come back to me!"
+tags: [4 directions, inwards]
+epistemic:
+  status: 4. Awaiting your feedback to finalise it.
+  effort: 3. It's been heavy (haha!), but seriously it wrote fairly smoothly.
+draft: true
+---
+
+One exercise during an Alexander Technique weekend: we sat looking at a blue yoga block on the floor. We related to it in these four different ways, and felt the shift in our bodies in response.
+
+I felt so moved by it that I wrote this letter to the workshop leader afterwards.
+
+---
+
+Dear Celia,
+
+Thank you so much for coming over to London last weekend to share your insights and delights with us.
+
+I often find that, after leaving a while to process events like yours, I don’t have the words to describe my learnings.
+
+Today I wanted to think deliberately about the weekend, starting with the blue yoga block game we played: wanting, not-wanting, neither-wanting-nor-not-wanting, wanting-and-allowing.
+
+That gave me such a shock of delight: how luscious it was to be able to want the block and wait for it to (re-)turn to me. It immediately reminded me of our youngest daughter.
+
+She’s always been very tactile, but most often backs herself into my embrace, so that her back is pressing against my front and she can watch the world whilst being supported by me. It is even more special than a normal hug.
+
+She does this time and time again - at the end of the school day, she runs out into the playground with her friends, a screamed greeting to me as she whizzes by, and plays until she tires, needs refreshment or contact. Then she comes to me and nestles in, immediately turning to watch the world from her point of safety.
+
+I have learned to know that she will come in her own good time, and that she is always mine to love and hold. I have learned to enjoy even more her busy-ness with the other children and parents, knowing that she will return home when the timing’s right.
+
+And so, the blue block.
+
+> ## Wanting
+>
+> Wanting it transformed into loving it, into a parental delight at its wish to stay out in the world a while longer before returning home to me.
+
+> ## Wanting and not-wanting
+>
+> I learned that I thwart wanting - it’s not allowed because it’s dangerous, it divides me. I learned that I am safer when not-wanting, that is a more coherent body. In the first state, my heart aches at its imprisonment in my chest; in the second state, my heart quietens, and the gaoler is now my protector.
+
+> ## Neither-wanting-nor-not-wanting
+>
+> Neither-wanting-nor-not-wanting allowed the block its own personality - no longer the object of my desires or repulsion, it could be itself and I could be respectfully curious. Ah, that’s full of ease. I could be with it, without being pulled into its orbit or repulsed away from it. I could be in its space; it could be in mine.
+
+> ## Wanting and allowing
+>
+> And the final, wondrous state. Peace. Delight. Relationships of love without attachment (though I am very attached to my daughters!). Enjoying without needing affirmation. And knowing, deeply knowing, that the right thing will happen when the time is appropriate. What a relief!
