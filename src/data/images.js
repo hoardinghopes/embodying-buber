f853d2da537e89aa77273b07b121b75290b4953e
James Knight <tech@jamesknightcoaching.com>
2021-03-19 16:53:34 +0000

    Now 11ty knows about the thumbnails.

--- src/data/images.js
+++ src/data/images.js
@@ -1,31 +1,31 @@
 module.exports = {
-  /*stats: {
-    src: "/images/backgrounds/chris-liverani-dBI_My696Rk-unsplash.jpg",
-    attribute:
-      '<span>Background photo by <a href="https://unsplash.com/@chrisliverani?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Chris Liverani</a> on <a href="/s/photos/statistics?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a></span>',
-  },*/
   about: {
     src: "/images/backgrounds/v2osk-Chi53DEUJ84-unsplash.jpg",
+    thumb: "/images/thumbs/v2osk-Chi53DEUJ84-unsplash.jpg",
     attribute:
       '<span>Background photo by <a href="https://unsplash.com/@v2osk?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">v2osk</a> on <a href="https://unsplash.com/s/visual/0c296cda-2158-4cc9-843d-7b1792091157?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Unsplash</a></span>',
   },
   contact: {
     src: "/images/backgrounds/camille-orgel-SKcTKYNRvHY-unsplash.jpg",
+    thumb: "/images/thumbs/camille-orgel-SKcTKYNRvHY-unsplash.jpg",
     attribute:
       '<span>Background photo by <a href="https://unsplash.com/@cam_bam?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Camille Orgel</a> on <a href="https://unsplash.com/s/photos/writing-letter?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Unsplash</a></span>',
   },
   thanks: {
     src: "/images/backgrounds/sandra-grunewald-pL6Z17faozk-unsplash.jpg",
+    thumb: "/images/thumbs/sandra-grunewald-pL6Z17faozk-unsplash.jpg",
     attribute:
       '<span>Background photo by <a href="https://unsplash.com/@elmuff?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Sandra Grünewald</a> on <a href="https://unsplash.com/s/photos/hearts?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Unsplash</a></span>',
   },
   "post-1": {
     src: "/images/backgrounds/mel-R4bsmYIc6FI-unsplash.jpg",
+    thumb: "/images/thumbs/mel-R4bsmYIc6FI-unsplash.jpg",
     attribute:
       '<span>Background photo by <a href="https://unsplash.com/@melindapack?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Mel</a> on <a href="https://unsplash.com/s/visual/131065db-23df-45b0-a58f-0024280938f8?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Unsplash</a></span>',
   },
   "post-2": {
     src: "/images/backgrounds/sharon-mccutcheon-om4O_x_qWD8-unsplash.jpg",
+    thumb: "/images/thumbs/sharon-mccutcheon-om4O_x_qWD8-unsplash.jpg",
     attribute:
       '<span>Background photo by <a href="https://unsplash.com/@sharonmccutcheon?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Sharon McCutcheon</a> on <a href="https://unsplash.com/s/visual/a90c41e3-9993-47c7-8e84-34e01750628b?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Unsplash</a></span>',
   },
@@ -33,137 +33,181 @@ module.exports = {
   "post-3": {
     src:
       "/images/backgrounds/city-church-christchurch-0sfPFlBcciA-unsplash.jpg",
+    thumb: "/images/thumbs/city-church-christchurch-0sfPFlBcciA-unsplash.jpg",
     attribute:
       '<span>Background photo by <a href="https://unsplash.com/@citychurchchristchurch?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">City Church Christchurch</a> on <a href="https://unsplash.com/s/photos/swarm?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Unsplash</a></span>',
   },
   "post-4": {
     src: "/images/backgrounds/ankush-minda-TLBplYQvqn0-unsplash.jpg",
+    thumb: "/images/thumbs/ankush-minda-TLBplYQvqn0-unsplash.jpg",
     attribute:
       '<span>Background photo by <a href="https://unsplash.com/@an_ku_sh?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Ankush Minda</a> on <a href="https://unsplash.com/s/photos/release?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Unsplash</a></span>',
   },
   "post-5": {
     src: "/images/backgrounds/milada-vigerova-0ArA_q8Gjt8-unsplash.jpg",
+    thumb: "/images/thumbs/milada-vigerova-0ArA_q8Gjt8-unsplash.jpg",
     attribute:
       '<span>Background photo by <a href="https://unsplash.com/@milada_vigerova?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Milada Vigerova</a> on <a href="https://unsplash.com/s/photos/heavy-ropes?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Unsplash</a></span>',
   },
   "post-6": {
     src: "/images/backgrounds/david-clode-1UBIvjKygYc-unsplash.jpg",
+    thumb: "/images/thumbs/david-clode-1UBIvjKygYc-unsplash.jpg",
     attribute:
       '<span>Photo by <a href="https://unsplash.com/@davidclode?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">David Clode</a> on <a href="https://unsplash.com/?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Unsplash</a></span>',
   },
   "post-7": {
     src: "/images/backgrounds/susan-yin-Ctaj_HCqW84-unsplash.jpg",
+    thumb: "/images/thumbs/susan-yin-Ctaj_HCqW84-unsplash.jpg",
     attribute:
       '<span>Background photo by <a href="https://unsplash.com/@syinq?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Susan Yin</a> on <a href="https://unsplash.com/s/photos/maze?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Unsplash</a></span>',
   },
   "post-8": {
     src: "/images/backgrounds/sistine-chapel-hand-of-god.jpg",
+    thumb: "/images/thumbs/sistine-chapel-hand-of-god.jpg",
     attribute: "",
   },
   "post-9": {
     src: "/images/backgrounds/marek-piwnicki-S07d0_p_xag-unsplash.jpg",
+    thumb: "/images/thumbs/marek-piwnicki-S07d0_p_xag-unsplash.jpg",
     attribute:
       '<span>Background photo by <a href="https://unsplash.com/@marekpiwnicki?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Marek Piwnicki</a> on <a href="https://unsplash.com/s/photos/spinning-top?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Unsplash</a></span>',
   },
   "post-10": {
     src: "/images/backgrounds/fallon-michael-dls5GJPHqVI-unsplash.jpg",
+    thumb: "/images/thumbs/fallon-michael-dls5GJPHqVI-unsplash.jpg",
     attribute:
       '<span>Background photo by <a href="https://unsplash.com/@fallonmichaeltx?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Fallon Michael</a> on <a href="https://unsplash.com/s/visual/dc3d70ca-f501-4942-8633-8e72827d93eb?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Unsplash</a></span>',
   },
-
   "post-11": {
     src: "/images/backgrounds/julia-sullivan-zwCfvu1kRgI-unsplash.jpg",
+    thumb: "/images/thumbs/julia-sullivan-zwCfvu1kRgI-unsplash.jpg",
     attribute:
       '<span>Background photo by <a href="https://unsplash.com/@juliasssullivan?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Julia Sullivan</a> on <a href="https://unsplash.com/s/photos/beach-seaweed?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Unsplash</a></span>',
   },
   "post-12": {
     src: "/images/backgrounds/michael-podger-hvm8k-44UOE-unsplash.jpg",
+    thumb: "/images/thumbs/michael-podger-hvm8k-44UOE-unsplash.jpg",
     attribute:
       '<span>Background photo by <a href="https://unsplash.com/@jammypodger7470?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">michael podger</a> on <a href="https://unsplash.com/@jammypodger7470?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Unsplash</a></span>',
   },
   "post-13": {
     src: "/images/backgrounds/gordon-williams-uxyDxSsKIY4-unsplash.jpg",
+    thumb: "/images/thumbs/gordon-williams-uxyDxSsKIY4-unsplash.jpg",
     attribute:
       '<span>Background photo by <a href="https://unsplash.com/@artifactflash?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Gordon Williams</a> on <a href="https://unsplash.com/s/photos/harris-and-lewis?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Unsplash</a></span>',
   },
   "post-14": {
     src: "/images/backgrounds/siora-photography-q5XDX1CadN4-unsplash.jpg",
+    thumb: "/images/thumbs/siora-photography-q5XDX1CadN4-unsplash.jpg",
     attribute:
       '<span>Background photo by <a href="https://unsplash.com/@siora18?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Siora Photography</a> on <a href="https://unsplash.com/s/photos/handwriting?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Unsplash</a></span>',
   },
   "post-15": {
     src: "/images/backgrounds/clay-banks-ZlaF6thdCgI-unsplash.jpg",
+    thumb: "/images/thumbs/clay-banks-ZlaF6thdCgI-unsplash.jpg",
     attribute:
       '<span>Background photo by <a href="https://unsplash.com/@claybanks?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Clay Banks</a> on <a href="https://unsplash.com/s/photos/i-love-you?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Unsplash</a></span>',
   },
   "post-16": {
     src: "/images/backgrounds/seb-cumberbirch-ktG-0sCXc34-unsplash.jpg",
+    thumb: "/images/thumbs/seb-cumberbirch-ktG-0sCXc34-unsplash.jpg",
     attribute:
       '<span>Background photo by <a href="https://unsplash.com/@sebcumberbirch?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Seb Cumberbirch</a> on <a href="https://unsplash.com/s/photos/tea?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Unsplash</a></span>',
   },
   "post-17": {
     src: "/images/backgrounds/clay-banks-LjqARJaJotc-unsplash.jpg",
+    thumb: "/images/thumbs/clay-banks-LjqARJaJotc-unsplash.jpg",
     attribute:
       '<span>Photo by <a href="https://unsplash.com/@claybanks?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Clay Banks</a> on <a href="https://unsplash.com/s/photos/hands?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Unsplash</a></span>',
   },
   "post-18": {
     src: "/images/backgrounds/possessed-photography-_E1PQXKUkMw-unsplash.jpg",
+    thumb: "/images/thumbs/possessed-photography-_E1PQXKUkMw-unsplash.jpg",
     attribute:
       '<span>Background photo by <a href="https://unsplash.com/@possessedphotography?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Possessed Photography</a> on <a href="https://unsplash.com/s/photos/target?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Unsplash</a></span>',
   },
   "post-19": {
     src: "/images/backgrounds/darv-w0qIP9tosbw-unsplash.jpg",
+    thumb: "/images/thumbs/darv-w0qIP9tosbw-unsplash.jpg",
     attribute:
       '<span>Background photo by <a href="https://unsplash.com/@darv?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Darv</a> on <a href="https://unsplash.com/s/photos/dark-corridor?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Unsplash</a></span>',
   },
   "post-20": {
     src: "/images/backgrounds/adria-berrocal-forcada-KtSnEXCe5U4-unsplash.jpg",
+    thumb: "/images/thumbs/adria-berrocal-forcada-KtSnEXCe5U4-unsplash.jpg",
     attribute:
       '<span>Photo by <a href="https://unsplash.com/@adrianikoos?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Adria Berrocal Forcada</a> on <a href="https://unsplash.com/collections/1536832/three?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Unsplash</a></span>',
   },
   "post-21": {
-    src: "/images/backgrounds/travel-sourced-W44F9FL9hmE-unsplash.jpg",
+    src: "/images/backgrounds/kon-karampelas-W44F9FL9hmE-unsplash.jpg",
+    thumb: "/images/thumbs/kon-karampelas-W44F9FL9hmE-unsplash.jpg",
     attribute:
       '<span>Photo by <a href="https://unsplash.com/@travelsourced?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Travel Sourced</a> on <a href="https://unsplash.com/?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Unsplash</a></span>',
   },
   "post-22": {
     src: "/images/backgrounds/cecile-hournau-GDyJRbm6Msg-unsplash.jpg",
+    thumb: "/images/thumbs/cecile-hournau-GDyJRbm6Msg-unsplash.jpg",
     attribute:
       '<span>Background photo by <a href="https://unsplash.com/@cecile_hournau?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Cecile Hournau</a> on <a href="https://unsplash.com/s/photos/french?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Unsplash</a></span>',
   },
   "post-23": {
     src: "/images/backgrounds/clem-onojeghuo-DoA2duXyzRM-unsplash.jpg",
+    thumb: "/images/thumbs/clem-onojeghuo-DoA2duXyzRM-unsplash.jpg",
     attribute:
-      '<span>Background photo by <a href="https://unsplash.com/@clemono?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Clem Onojeghuo</a> on <a href="/s/photos/loudspeaker?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a></span>',
+      '<span>Background photo by <a href="https://unsplash.com/@clemono?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Clem Onojeghuo</a> on <a href="https://unsplash.com/s/photosloudspeaker?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a></span>',
   },
   "post-24": {
     src: "/images/backgrounds/milada-vigerova-0ArA_q8Gjt8-unsplash.jpg",
+    thumb: "/images/thumbs/milada-vigerova-0ArA_q8Gjt8-unsplash.jpg",
     attribute:
       '<span>Background photo by <a href="https://unsplash.com/@milada_vigerova?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Milada Vigerova</a> on <a href="https://unsplash.com/s/photos/heavy-ropes?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Unsplash</a></span>',
   },
   "post-25": {
     src: "/images/backgrounds/raychan-vHf8wYS2aeY-unsplash.jpg",
+    thumb: "/images/thumbs/raychan-vHf8wYS2aeY-unsplash.jpg",
     attribute:
-      '<span>Photo by <a href="https://unsplash.com/@wx1993?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Raychan</a> on <a href="/s/photos/handwriting?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a></span>',
+      '<span>Photo by <a href="https://unsplash.com/@wx1993?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Raychan</a> on <a href="https://unsplash.com/s/photos/handwriting?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a></span>',
   },
   "post-26": {
     src: "/images/backgrounds/joshua-woroniecki-fjJmi9Edn4E-unsplash.jpg",
+    thumb: "/images/thumbs/joshua-woroniecki-fjJmi9Edn4E-unsplash.jpg",
     attribute:
-      '<span>Photo by <a href="https://unsplash.com/@joshua_j_woroniecki?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Joshua Woroniecki</a> on <a href="/s/photos/stars-at-night?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a></span>',
+      '<span>Photo by <a href="https://unsplash.com/@joshua_j_woroniecki?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Joshua Woroniecki</a> on <a href="https://unsplash.com/s/photos/stars-at-night?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a></span>',
   },
   "post-27": {
     src: "/images/backgrounds/michael-dziedzic-aQYgUYwnCsM-unsplash.jpg",
+    thumb: "/images/thumbs/michael-dziedzic-aQYgUYwnCsM-unsplash.jpg",
     attribute:
-      '<span>Photo by <a href="https://unsplash.com/@lazycreekimages?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Michael Dziedzic</a> on <a href="/s/photos/artificial-intelligence?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a></span>',
+      '<span>Photo by <a href="https://unsplash.com/@lazycreekimages?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Michael Dziedzic</a> on <a href="https://unsplash.com/s/photos/artificial-intelligence?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a></span>',
   },
   "post-28": {
     src: "/images/backgrounds/madhuvan-yadav-M-Tlne2HXmc-unsplash.jpg",
+    thumb: "/images/thumbs/madhuvan-yadav-M-Tlne2HXmc-unsplash.jpg",
     attribute:
-      '<span>Photo by <a href="https://unsplash.com/@madhuvan_yadav?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Madhuvan Yadav</a> on <a href="/s/photos/meteor?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a></span>',
+      '<span>Photo by <a href="https://unsplash.com/@madhuvan_yadav?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Madhuvan Yadav</a> on <a href="https://unsplash.com/s/photos/meteor?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a></span>',
   },
   "post-29": {
     src: "/images/backgrounds/mike-erskine-fvf0SoFeFpQ-unsplash.jpg",
+    thumb: "/images/thumbs/mike-erskine-fvf0SoFeFpQ-unsplash.jpg",
     attribute:
-      '<span>Photo by <a href="https://unsplash.com/@mikejerskine?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Mike Erskine</a> on <a href="/s/photos/brecon-beacons?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a></span>',
+      '<span>Photo by <a href="https://unsplash.com/@mikejerskine?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Mike Erskine</a> on <a href="https://unsplash.com/s/photos/brecon-beacons?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a></span>',
+  },
+  "post-71": {
+    src: "/images/backgrounds/ian-dooley-DuBNA1QMpPA-unsplash.jpg",
+    thumb: "/images/thumbs/ian-dooley-DuBNA1QMpPA-unsplash.jpg",
+    attribute:
+      '<span>Photo by <a href="https://unsplash.com/@sadswim?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">ian dooley</a> on <a href="/s/photos/travel-sourced-w44f9fl9hme-unsplash?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a></span>',
+  },
+  "post-72": {
+    src: "/images/backgrounds/himesh-kumar-behera-t11oyf1K8kA-unsplash.jpg",
+    thumb: "/images/thumbs/himesh-kumar-behera-t11oyf1K8kA-unsplash.jpg",
+    attribute:
+      '<span>Photo by <a href="https://unsplash.com/@whoishimesh?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Himesh Kumar Behera</a> on <a href="/s/photos/travel-sourced-w44f9fl9hme-unsplash?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a></span>',
+  },
+  "post-73": {
+    src: "/images/backgrounds/alexander-sinn-YYUM2sNvnvU-unsplash.jpg",
+    thumb: "/images/thumbs/alexander-sinn-YYUM2sNvnvU-unsplash.jpg",
+    attribute:
+      '<span>Photo by <a href="https://unsplash.com/@swimstaralex?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Alexander Sinn</a> on <a href="/s/photos/travel-sourced-w44f9fl9hme-unsplash?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a></span>',
   },
 };
